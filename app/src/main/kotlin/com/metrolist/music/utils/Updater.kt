05f925723993878eab3b4f7c39497c8a6f1dbc91
Narutoooo_Dev <flashstore801@gmail.com>
2026-05-06 04:33:30 +0300

    Change updater source

--- app/src/main/kotlin/com/metrolist/music/utils/Updater.kt
+++ app/src/main/kotlin/com/metrolist/music/utils/Updater.kt
@@ -39,7 +39,7 @@ object Updater {
     private var cachedAllReleases: List<ReleaseInfo> = emptyList()
     
     private const val CHECK_INTERVAL_MILLIS = 2 * 60 * 60 * 1000L // 2 hours
-    private const val GITHUB_API_BASE = "https://api.github.com/repos/MetrolistGroup/Metrolist"
+    private const val GITHUB_API_BASE = "https://api.github.com/repos/narutoooo-dev/Metrolist-Latest"
 
     /**
      * Compares two version strings.
